333ab1edde3910086a0bcceb2cbd4f301aa883ae
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-05-25 16:20:40 +0530

    Sarvam AI client for php

---
+++ src/Sarvam.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Sarvam;
+
+class Sarvam {
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ tests/Tests.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+namespace Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Sarvam;
+
+#[CoversClass(Sarvam::class)]
+class Tests extends TestCase
+{
+    public function connect(): void
+    {
+
+    }
+}
